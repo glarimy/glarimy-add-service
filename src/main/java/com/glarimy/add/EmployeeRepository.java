4e1df7e08d07b73a7d00f537a6ca1b6fe22281c8
Krishna Mohan Koyya <krishna@glarimy.com>
2019-03-25 17:21:03 +0530

    spring cloud microservices

--- src/main/java/com/glarimy/add/EmployeeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.glarimy.add;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EmployeeRepository extends MongoRepository<Employee, String>{
-
-}
